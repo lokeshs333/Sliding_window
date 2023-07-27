880ba41bde016d0c19a45d0cce2595420f3f6268
LOKESH PANDEY <73874665+lokeshs333@users.noreply.github.com>
2023-07-27 15:37:12 +0530

    Add files via upload

---
+++ sw1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int maxsubarray(int *arr, int n, int k)
+{
+    int i = 0, j = 0, cursum = 0, maxsum = 0;
+    while (j < n)
+    {
+        cursum += arr[j];
+        if (j - i + 1 < k)
+            j++;
+        else if (j - i + 1 == k)
+        {
+            maxsum = max(maxsum, cursum);
+            cursum = cursum - arr[i];
+            i++;
+            j++;
+        }
+    }
+    return maxsum;
+}
+int main()
+{
+    int n = 7;
+    int k = 3;
+    int arr[] = {2, 3, 5, 2, 9, 7, 1};
+    cout << maxsubarray(arr, n, k);
+    return 0;
+}
